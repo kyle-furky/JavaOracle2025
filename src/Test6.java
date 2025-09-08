788ba4d7c2f6eb8ff24b59aa0534dcfdb46b3d79
kyle-furky <kylefurky@gmail.com>
2025-09-07 20:36:02 -0400

    oracle q&a - sunday evening

---
+++ src/Test6.java
@@ -0,0 +1,18 @@
+public class Test6 {
+
+
+    public static void main(String[]args){
+
+    int count =0;
+    for(int i =0; i<3; i++){
+        for (int j =0; i<2; i++)
+            count ++;
+
+
+        }
+        System.out.println(count);
+    }
+
+
+
+}
